8c67a576a940b9b306f6ac27f77f7ac8d8eb4226
Усанов Дмитрий Павлович <usanovdp@gmail.com>
2022-05-29 19:46:25 +0300

    Добавьте файлы проекта.

---
+++ ConsoleAppPostgre/ApplicationContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleAppPostgre
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; } = null!;
+
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=postgres;Username=postgres;Password=admin");
+        }
+    }
+}
+
---
+++ ConsoleAppPostgre/Program.cs
@@ -0,0 +1,26 @@
+﻿using ConsoleAppPostgre;
+
+// добавление данных
+using (ApplicationContext db = new ApplicationContext())
+{
+    // создаем два объекта User
+    User user1 = new User { Name = "Tom", Age = 33 };
+    User user2 = new User { Name = "Alice", Age = 26 };
+    User dima = new User { Name = "Дима", Age = 54 };
+    User olga = new User { Name = "Ольга", Age = 45 };
+
+    // добавляем их в бд
+    db.Users.AddRange(user1, user2, dima, olga);
+    db.SaveChanges();
+//}
+// получение данных
+//using (ApplicationContext db = new ApplicationContext())
+//{
+    // получаем объекты из бд и выводим на консоль
+    var users = db.Users.ToList();
+    Console.WriteLine("Users list:");
+    foreach (User u in users)
+    {
+        Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+    }
+}
